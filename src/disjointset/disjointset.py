035ff9367e4c2e95e2a26b1eeb69a6b51112d261
CRISvsGAME <cris@crisvsgame.com>
2025-06-18 20:00:00 +0000

    DisjointSet 0.0.4
    Add DisjointSet Class

--- src/disjointset/disjointset.py
+++ src/disjointset/disjointset.py
@@ -1,1 +1,29 @@
 """Disjoint Set (Union Find) Data Structure."""
+
+from __future__ import annotations
+from typing import Generic, TypeVar
+from collections.abc import Hashable
+
+T = TypeVar("T", bound=Hashable)
+
+
+class DisjointSet(Generic[T]):
+    """
+    A generic, type-safe Disjoint Set Union (Union-Find) data structure.
+
+    Supports:
+        Core Operations:
+            - make_set(x)
+            - find(x)
+            - union(x, y)
+            - same_set(x, y)
+        Convenience Helpers:
+            - make_set_many([x, y, z])
+            - find_many([x, y, z])
+            - union_many([x, y, z])
+            - same_set_many([x, y, z])
+    """
+
+    def __init__(self) -> None:
+        self.parent: dict[T, T] = {}
+        self.rank: dict[T, int] = {}
