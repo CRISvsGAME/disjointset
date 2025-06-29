639477a811329c234a7486a492857dbdaec39a72
CRISvsGAME <cris@crisvsgame.com>
2025-06-29 20:00:00 +0000

    DisjointSet 0.0.15
    Add Convenience Helpers Tests

--- tests/disjointset/test_disjointset.py
+++ tests/disjointset/test_disjointset.py
@@ -44,3 +44,46 @@ def test_same_set():
     dsu.union(2, 3)
     assert dsu.same_set(1, 3)
     assert not dsu.same_set(1, 4)
+
+
+# ------------------------------------------------------------------------------
+# Convenience Helpers Tests
+# ------------------------------------------------------------------------------
+
+
+def test_make_set_many():
+    """Test make_set_many operation."""
+    dsu = DisjointSet[int]()
+    dsu.make_set_many([1, 2, 3])
+    assert dsu.find_many([1, 2, 3]) == (1, 2, 3)
+
+
+def test_find_many():
+    """Test find_many operation."""
+    dsu = DisjointSet[int]()
+    dsu.make_set_many([1, 2, 3])
+    dsu.union_many([1, 2])
+    roots = dsu.find_many([1, 2, 3])
+    assert len(roots) == 3
+    assert roots[0] == roots[1]
+    assert roots[0] != roots[2]
+
+
+def test_union_many():
+    """Test union_many operation."""
+    dsu = DisjointSet[int]()
+    dsu.make_set_many([1, 2, 3, 4])
+    dsu.union_many([1, 2, 3])
+    assert dsu.same_set_many([1, 2, 3])
+    assert not dsu.same_set_many([1, 2, 3, 4])
+
+
+def test_same_set_many():
+    """Test same_set_many operation."""
+    dsu = DisjointSet[int]()
+    dsu.make_set_many([1, 2, 3, 4, 5])
+    dsu.union_many([1, 2, 3])
+    dsu.union_many([4, 5])
+    assert dsu.same_set_many([1, 2, 3])
+    assert dsu.same_set_many([4, 5])
+    assert not dsu.same_set_many([1, 2, 3, 4, 5])
